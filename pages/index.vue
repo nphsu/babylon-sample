26bee3965b18a0d16eb93963e4d8bafb0c2c7714
Shunpei Koike <shunpei42ba@gmail.com>
2019-06-25 14:49:27 +0900

    add texture

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,26 @@
   div
     Scene
       Box(:position="[0, 0, 5]")
+        Material(diffuse="#843",alpha="0.5")
+      Ground(:src="background")
+    //- Sphere
+    //-   Material
+    //-     Texture(type="emissive" :src="background")
     p This is the 3D object.
 </template>
 
 <script>
 import Vue from 'vue'
 import vb from 'vue-babylonjs'
+import Background from '~/assets/image/background.png'
 Vue.use(vb)
 export default {
   components: {
+  },
+  data () {
+    return {
+      background: Background
+    }
   }
 }
 </script>
